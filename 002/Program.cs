573dcf1cdf49b28a31b2d68284462f4f1d8dcdc8
Astr <boxreg2000@yandex.ru>
2022-02-02 11:37:39 +0300

    2 задача расширена до десятичных чисел

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,8 +1,8 @@
 ﻿//2.Даны два числа. Показать большее и меньшее число
 Console.WriteLine("Введите первое число");
-int n=int.Parse(Console.ReadLine());
+double n=double.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int m=int.Parse(Console.ReadLine());
+double m=double.Parse(Console.ReadLine());
 if (n>m)
 {
     Console.WriteLine("Число "+n+" больше числа "+m);
